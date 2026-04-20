dee7c4ab35a744489d77f73c567ed1ab1073d17b
Ahmed Abutahoun <nexusrexdev@gmail.com>
2026-04-21 01:27:01 +0200

    MusicEntry stream validation

--- AudioManager/Scripts/Resources/AudioEntries/MusicEntry.gd
+++ AudioManager/Scripts/Resources/AudioEntries/MusicEntry.gd
@@ -1,7 +1,23 @@
+@tool
 class_name MusicEntry extends Resource
 
 @export_group("Track Data")
-@export_file("*.ogg", "*.mp3", "*.wav", "*.tres") var stream_path: String = ""
+@export_file("*.ogg", "*.mp3", "*.wav", "*.tres") var stream_path: String = "":
+    set(value):
+        stream_path = value
+        _validate_stream()
 
 @export_group("Mixing")
 @export_range(-80.0, 24.0) var volume_offset: float = 0.0
+
+func _validate_stream() -> void:
+    if not Engine.is_editor_hint() or stream_path.is_empty():
+        return
+        
+    if stream_path.ends_with(".tres") or stream_path.ends_with(".res"):
+        var res_type: String = ResourceLoader.get_resource_type(stream_path)
+        
+        if not ClassDB.is_parent_class(res_type, "AudioStream"):
+            printerr("MusicEntry Error: The file '%s' is a '%s', NOT an AudioStream!" % [stream_path.get_file(), res_type])
+            stream_path = ""
+            notify_property_list_changed()

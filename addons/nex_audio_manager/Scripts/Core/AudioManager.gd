628e7e88de069fa6bec44c8e8f48efe074f4ed2d
Ahmed Abutahoun <nexusrexdev@gmail.com>
2026-09-26 02:46:45 +0300

    Migrated to class-based Autoload, configs in ProjectSettings and bus dict

--- addons/nex_audio_manager/Scripts/Core/AudioManager.gd
+++ addons/nex_audio_manager/Scripts/Core/AudioManager.gd
@@ -7,27 +7,39 @@ var _sfx_pool_global: NonSpatialAudioPool
 var _ui_audio_pool: NonSpatialAudioPool
 var _voiceline_pool: NonSpatialAudioPool
 
-@export var _config: AudioConfig
+var _bus_cache: Dictionary[AudioEnums.Buses, int] = {}
+
+var _config: AudioConfig
+
+var _missing_data: bool = false
 
 func _ready() -> void:
-	if not _config:
-		printerr("AudioManager Error: No AudioConfig resource assigned.")
+	var config_path = ProjectSettings.get_setting("nex_audio_manager/active_config")
+	if ResourceLoader.exists(config_path):
+		_config = ResourceLoader.load(config_path)
+	else:
+		printerr("AudioManager Error: No AudioConfig resource found at path: %s" % config_path)
+		_missing_data = true
 		return
 	
 	if not _config.music_registry:
 		printerr("AudioManager Error: No MusicRegistry resource assigned.")
+		_missing_data = true
 		return
 
 	if not _config.sfx_registry:
 		printerr("AudioManager Error: No SFXRegistry resource assigned.")
+		_missing_data = true
 		return
 
 	if not _config.voiceline_registry:
 		printerr("AudioManager Error: No VoicelineRegistry resource assigned.")
+		_missing_data = true
 		return
 
 	if not _config.ui_registry:
 		printerr("AudioManager Error: No UIAudioRegistry resource assigned.")
+		_missing_data = true
 		return
 	
 	if _config.enable_music:
@@ -56,6 +68,8 @@ func _ready() -> void:
 		add_child(_voiceline_pool)
 		_voiceline_pool.voice_ended.connect(_handle_voiceline_ended)
 
+	_generate_bus_cache()
+
 func _notification(what: int) -> void:
 	match what:
 		NOTIFICATION_PAUSED:
@@ -64,7 +78,7 @@ func _notification(what: int) -> void:
 			_toggle_pause_effects(false)
 
 func _toggle_pause_effects(paused: bool) -> void:
-	if not _config:
+	if _missing_data:
 		return
 
 	if paused:
@@ -78,6 +92,9 @@ func _toggle_pause_effects(paused: bool) -> void:
 				AudioServer.remove_bus_effect(AudioServer.get_bus_index("Master"), 0)
 
 func _handle_voiceline_ended(_player: AudioStreamPlayer) -> void:
+	if _missing_data:
+		return
+
 	if _config.enable_ducking_on_voiceline:
 		if _config.enable_music:
 			_music_engine.toggle_ducking(false)
@@ -91,10 +108,16 @@ func _handle_voiceline_ended(_player: AudioStreamPlayer) -> void:
 		if _config.enable_global_sfx:
 			_sfx_pool_global._set_ducking(false)
 
+func _generate_bus_cache() -> void:
+	for bus_enum in AudioEnums.Buses.values():
+		var bus_name: StringName = AudioEnums.Buses.keys()[bus_enum]
+		var bus_index: int = AudioServer.get_bus_index(bus_name)
+		_bus_cache[bus_enum] = bus_index
+
 #region Music Control
 
 func play_music(music_enum: AudioEnums.Music, crossfade_time: float = 0.0) -> void:
-	if not _config.enable_music:
+	if not _config.enable_music or _missing_data:
 		return
 	var entry: MusicEntry = _config.music_registry.get_entry(music_enum)
 	if not entry or not entry.stream:
@@ -103,32 +126,32 @@ func play_music(music_enum: AudioEnums.Music, crossfade_time: float = 0.0) -> vo
 	_music_engine.play(entry.stream, entry, crossfade_time)
 
 func pause_music() -> void:
-	if not _config.enable_music:
+	if not _config.enable_music or _missing_data:
 		return
 	_music_engine.pause()
 
 func unpause_music() -> void:
-	if not _config.enable_music:
+	if not _config.enable_music or _missing_data:
 		return
 	_music_engine.unpause()
 
 func stop_music(fadeout_time: float = 0.0) -> void:
-	if not _config.enable_music:
+	if not _config.enable_music or _missing_data:
 		return
 	_music_engine.stop(fadeout_time)
 
 func is_music_playing() -> bool:
-	if not _config.enable_music:
+	if not _config.enable_music or _missing_data:
 		return false
 	return _music_engine.is_playing()
 
 func switch_music_section(clip_name: StringName) -> void:
-	if not _config.enable_music:
+	if not _config.enable_music or _missing_data:
 		return
 	_music_engine.switch_section(clip_name)
 
 func toggle_music_stem(index: int, enable: bool, fade_time: float = 0.0) -> void:
-	if not _config.enable_music:
+	if not _config.enable_music or _missing_data:
 		return
 	_music_engine.toggle_stem(index, enable, fade_time)
 
@@ -137,7 +160,7 @@ func toggle_music_stem(index: int, enable: bool, fade_time: float = 0.0) -> void
 #region 3D SFX Control
 
 func play_sfx_3d_positioned(sfx_enum: AudioEnums.SFX, position: Vector3) -> void:
-	if not _config.enable_3d_sfx:
+	if not _config.enable_3d_sfx or _missing_data:
 		return
 	var entry: SFXEntry = _config.sfx_registry.get_entry(sfx_enum)
 	if not entry or not entry.stream:
@@ -146,7 +169,7 @@ func play_sfx_3d_positioned(sfx_enum: AudioEnums.SFX, position: Vector3) -> void
 	_sfx_pool_3d.play_positioned(entry.stream, entry, position)
 
 func play_sfx_3d_targeted(sfx_enum: AudioEnums.SFX, target: Node3D) -> void:
-	if not _config.enable_3d_sfx:
+	if not _config.enable_3d_sfx or _missing_data:
 		return
 	var entry: SFXEntry = _config.sfx_registry.get_entry(sfx_enum)
 	if not entry or not entry.stream:
@@ -159,7 +182,7 @@ func play_sfx_3d_targeted(sfx_enum: AudioEnums.SFX, target: Node3D) -> void:
 #region 2D SFX Control
 
 func play_sfx_2d_positioned(sfx_enum: AudioEnums.SFX, position: Vector2) -> void:
-	if not _config.enable_2d_sfx:
+	if not _config.enable_2d_sfx or _missing_data:
 		return
 	var entry: SFXEntry = _config.sfx_registry.get_entry(sfx_enum)
 	if not entry or not entry.stream:
@@ -168,7 +191,7 @@ func play_sfx_2d_positioned(sfx_enum: AudioEnums.SFX, position: Vector2) -> void
 	_sfx_pool_2d.play_positioned(entry.stream, entry, position)
 
 func play_sfx_2d_targeted(sfx_enum: AudioEnums.SFX, target: Node2D) -> void:
-	if not _config.enable_2d_sfx:
+	if not _config.enable_2d_sfx or _missing_data:
 		return
 	var entry: SFXEntry = _config.sfx_registry.get_entry(sfx_enum)
 	if not entry or not entry.stream:
@@ -181,7 +204,7 @@ func play_sfx_2d_targeted(sfx_enum: AudioEnums.SFX, target: Node2D) -> void:
 #region Global SFX Control
 
 func play_sfx_global(sfx_enum: AudioEnums.SFX) -> void:
-	if not _config.enable_global_sfx:
+	if not _config.enable_global_sfx or _missing_data:
 		return
 	var entry: SFXEntry = _config.sfx_registry.get_entry(sfx_enum)
 	if not entry or not entry.stream:
@@ -190,16 +213,16 @@ func play_sfx_global(sfx_enum: AudioEnums.SFX) -> void:
 	_sfx_pool_global.play(entry.stream, entry)
 
 func play_ui_audio(ui_audio_enum: AudioEnums.UI) -> void:
-	if not _config.enable_ui_audio:
+	if not _config.enable_ui_audio or _missing_data:
 		return
-	var entry: SFXEntry = _config.ui_audio_registry.get_entry(ui_audio_enum)
+	var entry: SFXEntry = _config.ui_registry.get_entry(ui_audio_enum)
 	if not entry or not entry.stream:
 		printerr("AudioManager Error: Invalid UIAudioEntry provided to play_ui_audio()")
 		return
 	_ui_audio_pool.play(entry.stream, entry)
 
 func play_voiceline(voiceline_enum: AudioEnums.Voiceline) -> void:
-	if not _config.enable_voicelines:
+	if not _config.enable_voicelines or _missing_data:
 		return
 	var entry: SFXEntry = _config.voiceline_registry.get_entry(voiceline_enum)
 	if not entry or not entry.stream:
@@ -222,31 +245,31 @@ func play_voiceline(voiceline_enum: AudioEnums.Voiceline) -> void:
 #region Bus Control
 
 func set_bus_volume(bus: AudioEnums.Buses, volume_db: float) -> void:
-	var bus_name: StringName = AudioEnums.Buses.keys()[bus]
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus_name), volume_db)
+	var bus_index: int = _bus_cache[bus]
+	AudioServer.set_bus_volume_db(bus_index, volume_db)
 
 func set_bus_mute(bus: AudioEnums.Buses, mute: bool) -> void:
-	var bus_name: StringName = AudioEnums.Buses.keys()[bus]
-	AudioServer.set_bus_mute(AudioServer.get_bus_index(bus_name), mute)
+	var bus_index: int = _bus_cache[bus]
+	AudioServer.set_bus_mute(bus_index, mute)
 
 func add_bus_effect(bus: AudioEnums.Buses, effect: AudioEffect, index: int) -> void:
-	var bus_name: StringName = AudioEnums.Buses.keys()[bus]
-	AudioServer.add_bus_effect(AudioServer.get_bus_index(bus_name), effect, index)
+	var bus_index: int = _bus_cache[bus]
+	AudioServer.add_bus_effect(bus_index, effect, index)
 
 func remove_bus_effect(bus: AudioEnums.Buses, effect_index: int) -> void:
-	var bus_name: StringName = AudioEnums.Buses.keys()[bus]
-	AudioServer.remove_bus_effect(AudioServer.get_bus_index(bus_name), effect_index)
+	var bus_index: int = _bus_cache[bus]
+	AudioServer.remove_bus_effect(bus_index, effect_index)
 
 func get_bus_effect_count(bus: AudioEnums.Buses) -> int:
-	var bus_name: StringName = AudioEnums.Buses.keys()[bus]
-	return AudioServer.get_bus_effect_count(AudioServer.get_bus_index(bus_name))
+	var bus_index: int = _bus_cache[bus]
+	return AudioServer.get_bus_effect_count(bus_index)
 
 func get_bus_effect(bus: AudioEnums.Buses, effect_index: int) -> AudioEffect:
-	var bus_name: StringName = AudioEnums.Buses.keys()[bus]
-	return AudioServer.get_bus_effect(AudioServer.get_bus_index(bus_name), effect_index)
+	var bus_index: int = _bus_cache[bus]
+	return AudioServer.get_bus_effect(bus_index, effect_index)
 
 func set_bus_effect_enabled(bus: AudioEnums.Buses, effect_index: int, enabled: bool) -> void:
-	var bus_name: StringName = AudioEnums.Buses.keys()[bus]
-	AudioServer.set_bus_effect_enabled(AudioServer.get_bus_index(bus_name), effect_index, enabled)
+	var bus_index: int = _bus_cache[bus]
+	AudioServer.set_bus_effect_enabled(bus_index, effect_index, enabled)
 
 #endregion
--- addons/nex_audio_manager/nex_audio_manager.gd
+++ addons/nex_audio_manager/nex_audio_manager.gd
@@ -1,60 +1,122 @@
 @tool
 extends EditorPlugin
 
-const AUDIO_MANAGER_PATH: String = "res://addons/nex_audio_manager/Scenes/audio_manager.tscn"
+const AUDIO_MANAGER_PATH: String = "res://addons/nex_audio_manager/Scripts/Core/AudioManager.gd"
 const BUS_LAYOUT_PATH: String = "res://addons/nex_audio_manager/Resources/bus_layout.tres"
+const CONFIG_SETTING_PATH: String = "nex_audio_manager/active_config"
 
 var _submenu: PopupMenu
+var _config_dialog: EditorFileDialog
 
 enum MenuAction {
-	SYNC_BUSES,
+    SYNC_BUSES,
 }
 
 func _enable_plugin() -> void:
-	add_autoload_singleton("AudioManager", AUDIO_MANAGER_PATH)
-	_setup_buses_from_enum()
+    add_autoload_singleton("AudioManager", AUDIO_MANAGER_PATH)
+    _setup_project_settings()
+    _setup_buses_from_enum()
 
 func _disable_plugin() -> void:
-	remove_autoload_singleton("AudioManager")
-	ProjectSettings.set_setting("audio/buses/default_bus_layout", "")
-	ProjectSettings.save()
+    remove_autoload_singleton("AudioManager")
+    
+    ProjectSettings.set_setting("audio/buses/default_bus_layout", "")
+    ProjectSettings.set_setting(CONFIG_SETTING_PATH, null)
+    ProjectSettings.save()
 
 func _enter_tree() -> void:
-	_submenu = PopupMenu.new()
-	_submenu.add_item("Sync Buses from Enum", MenuAction.SYNC_BUSES)
-	_submenu.id_pressed.connect(_on_submenu_id_pressed)
-	
-	add_tool_submenu_item("Nex Audio Manager", _submenu)
+    _submenu = PopupMenu.new()
+    _submenu.add_item("Sync Buses from Enum", MenuAction.SYNC_BUSES)
+    _submenu.id_pressed.connect(_on_submenu_id_pressed)
+    
+    add_tool_submenu_item("Nex's Audio Manager", _submenu)
 
 func _exit_tree() -> void:
-	remove_tool_menu_item("Nex Audio Manager")
-	if _submenu:
-		_submenu.queue_free()
+    remove_tool_menu_item("Nex's Audio Manager")
+    if _submenu:
+        _submenu.queue_free()
+    _cleanup_dialog()
 
 func _on_submenu_id_pressed(id: int) -> void:
-	match id:
-		MenuAction.SYNC_BUSES:
-			_setup_buses_from_enum()
+    match id:
+        MenuAction.SYNC_BUSES:
+            _setup_buses_from_enum()
 
 func _setup_buses_from_enum() -> void:
-	var keys: Array = AudioEnums.Buses.keys()
+    var keys: Array = AudioEnums.Buses.keys()
 
-	for key in keys:
-		var bus_name: String = key.capitalize()
-		if key in ["SFX", "UI"]:
-			bus_name = key
-			
-		if AudioServer.get_bus_index(bus_name) != -1:
-			continue
-			
-		var new_index: int = AudioServer.bus_count
-		AudioServer.add_bus(new_index)
-		AudioServer.set_bus_name(new_index, bus_name)
-		if new_index > 0:
-			AudioServer.set_bus_send(new_index, &"Master")
+    for key in keys:
+        var bus_name: String = key.capitalize()
+        if key in ["SFX", "UI"]:
+            bus_name = key
+            
+        if AudioServer.get_bus_index(bus_name) != -1:
+            continue
+            
+        var new_index: int = AudioServer.bus_count
+        AudioServer.add_bus(new_index)
+        AudioServer.set_bus_name(new_index, bus_name)
+        if new_index > 0:
+            AudioServer.set_bus_send(new_index, &"Master")
 
-	var layout: AudioBusLayout = AudioServer.generate_bus_layout()
-	ResourceSaver.save(layout, BUS_LAYOUT_PATH)
-	ProjectSettings.set_setting("audio/buses/default_bus_layout", BUS_LAYOUT_PATH)
-	ProjectSettings.save()
-	print("[NexAudioManager] Audio buses synced successfully.")
+    var layout: AudioBusLayout = AudioServer.generate_bus_layout()
+    ResourceSaver.save(layout, BUS_LAYOUT_PATH)
+    ProjectSettings.set_setting("audio/buses/default_bus_layout", BUS_LAYOUT_PATH)
+    ProjectSettings.save()
+    print("[NexAudioManager] Audio buses synced successfully.")
+
+func _setup_project_settings() -> void:
+    var current_path = ProjectSettings.get_setting(CONFIG_SETTING_PATH, "")
+    
+    if current_path == "" or not FileAccess.file_exists(current_path):
+        _prompt_for_config_save()
+    else:
+        _apply_setting_properties(current_path)
+
+func _prompt_for_config_save() -> void:
+    _config_dialog = EditorFileDialog.new()
+    _config_dialog.file_mode = EditorFileDialog.FILE_MODE_SAVE_FILE
+    _config_dialog.title = "Save Config File"
+    _config_dialog.add_filter("*.tres", "Godot Resource")
+    _config_dialog.current_file = "nex_audio_config.tres" 
+    _config_dialog.exclusive = false
+    
+    var base_control = EditorInterface.get_base_control()
+    base_control.add_child(_config_dialog)
+    
+    _config_dialog.file_selected.connect(_on_config_file_selected)
+    _config_dialog.canceled.connect(_on_config_dialog_canceled)
+    
+    _config_dialog.popup_centered_ratio(0.4)
+
+func _on_config_file_selected(path: String) -> void:
+    var new_config = AudioConfig.new()
+    ResourceSaver.save(new_config, path)
+    print("[NexAudioManager] Generated config at: ", path)
+    
+    _apply_setting_properties(path)
+    _cleanup_dialog()
+
+func _on_config_dialog_canceled() -> void:
+    push_warning("[NexAudioManager] Initialization canceled. You must manually create an AudioConfig and assign it in Project Settings -> Nex Audio Manager.")
+    _apply_setting_properties("")
+    _cleanup_dialog()
+
+func _apply_setting_properties(path: String) -> void:
+    ProjectSettings.set_setting(CONFIG_SETTING_PATH, path)
+    
+    ProjectSettings.add_property_info({
+        "name": CONFIG_SETTING_PATH,
+        "type": TYPE_STRING,
+        "hint": PROPERTY_HINT_FILE,
+        "hint_string": "*.tres"
+    })
+    
+    ProjectSettings.set_initial_value(CONFIG_SETTING_PATH, "")
+
+    ProjectSettings.save()
+
+func _cleanup_dialog() -> void:
+    if _config_dialog and is_instance_valid(_config_dialog):
+        _config_dialog.queue_free()
+        _config_dialog = null
